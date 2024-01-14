6d7259118b84e8893db19db0315f7fd51b333bb7
Halil <77546808+yilmazhalilibr@users.noreply.github.com>
2024-01-14 13:52:34 +0300

    Safe Commit
    I can spawn in scene to object but not true order.

--- Source/Unreal_Crafting/Private/MephistoSave.cpp
+++ Source/Unreal_Crafting/Private/MephistoSave.cpp
@@ -9,9 +9,10 @@
 #include "Engine/World.h"
 #include "Serialization/MemoryWriter.h"
 #include "Serialization/MemoryReader.h"
-#include "Misc/FileHelper.h"
 #include <Serialization/ObjectAndNameAsStringProxyArchive.h>
+#include <algorithm>
 
+////////////////////
 std::string ConvertFStringToStdString(const FString& InFString)
 {
 	std::string Result(TCHAR_TO_UTF8(*InFString));
@@ -23,6 +24,26 @@ FString ConvertStdStringToFString(const std::string& InStdString)
 	FString Result = FString(UTF8_TO_TCHAR(InStdString.c_str()));
 	return Result;
 }
+
+FString UMephistoSave::BytesArrayConvertToString(TArray<uint8> data)
+{
+	FString OutString;
+	if (data.Num() == 0)
+	{
+		OutString.Empty();
+	}
+
+	OutString = FString(data.Num(), reinterpret_cast<const TCHAR*>(data.GetData()));
+
+	return OutString;
+}
+
+// Yardımcı bir fonksiyon: Bytes dizisini FString'e dönüştürme
+FString UMephistoSave::StringFromBytes(const TArray<uint8>& Bytes)
+{
+	return FString(reinterpret_cast<const TCHAR*>(Bytes.GetData()), Bytes.Num());
+}
+////////////////////
 
 FString UMephistoSave::ReadFile(const FString& Directory, const FString& FileName, FString& OutData)
 {
@@ -72,7 +93,7 @@ TArray<uint8>  UMephistoSave::Serializeable(AActor* MyActor)
 	{
 		FMemoryWriter MemoryWriter(SerializedData);
 
-		FString ActorName = MyActor->GetName();
+		FString ActorName = MyActor->Tags[1].ToString();
 		FTransform ActorTransform = MyActor->GetTransform();
 		TArray<FName> ActorTags = MyActor->Tags;
 
@@ -80,6 +101,8 @@ TArray<uint8>  UMephistoSave::Serializeable(AActor* MyActor)
 		MemoryWriter << ActorName;
 		MemoryWriter << ActorTransform;
 		MemoryWriter << ActorTags;
+
+		UE_LOG(LogTemp, Warning, TEXT("Name %s"), *ActorName);
 
 		MemoryWriter.Close();
 	}
@@ -89,7 +112,7 @@ TArray<uint8>  UMephistoSave::Serializeable(AActor* MyActor)
 }
 
 
-void UMephistoSave::Deserializeable(const TArray<uint8>& SerializedData)
+AActor* UMephistoSave::Deserializeable(TArray<UClass*> classType, UWorld* World, const TArray<uint8>& SerializedData)
 {
 
 	FMemoryReader MemoryReader(SerializedData, true);
@@ -105,9 +128,128 @@ void UMephistoSave::Deserializeable(const TArray<uint8>& SerializedData)
 	MemoryReader.Close();
 
 
+	AActor* SpawnedActor = nullptr;
+
+	for (UClass* classData : classType)
+	{
+
+		if (classData->GetName().Contains("RightArm"))
+		{
+
+			UE_LOG(LogTemp, Warning, TEXT("Success"));
+
+			SpawnedActor = World->GetWorld()->SpawnActor<AActor>(classData);
+
+			SpawnedActor->SetActorLabel(ActorName);
+			SpawnedActor->SetActorTransform(ActorTransform);
+			SpawnedActor->Tags = CurrentTags;
+
+			break;
+		}
+		UE_LOG(LogTemp, Warning, TEXT("Failed To ClassData"));
+	}
+
+
+
+	//if (SpawnedActor)
+	//{
+	//	SpawnedActor->SetActorLabel(ActorName);
+	//	SpawnedActor->SetActorTransform(ActorTransform);
+	//	SpawnedActor->Tags = CurrentTags;
+	//}
+
+	return SpawnedActor;
 }
 
-std::tuple<int, int> MyFnc()
+
+TArray<uint8>  UMephistoSave::Serializeables(TArray<AActor*> MyActors)
 {
+	TArray<uint8> SerializedData;
+
+	if (MyActors.Num() != 0)
+	{
+		FMemoryWriter MemoryWriter(SerializedData);
+
+		for (AActor* actor : MyActors)
+		{
+			FString ActorName = actor->GetName();
+			FTransform ActorTransform = actor->GetTransform();
+			TArray<FName> ActorTags = actor->Tags;
+
+			MemoryWriter << ActorName;
+			MemoryWriter << ActorTransform;
+			MemoryWriter << ActorTags;
+		}
+
+		MemoryWriter.Close();
+	}
+
+	return SerializedData;
 
 }
+
+bool UMephistoSave::WriteDataToFile(const FString& Directory, const FString& FileName, const TArray<FString>& Data)
+{
+	FString FullPath = FPaths::Combine(Directory, FileName);
+
+	// Dosyayı yazma işlemi
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+
+	// Binary dosyayı yazma modunda açma
+	IFileHandle* FileHandle = PlatformFile.OpenWrite(*FullPath, true);
+	if (FileHandle)
+	{
+		// Verileri dosyaya yazma
+		for (const FString& Line : Data)
+		{
+			FString LineWithNewLine = Line + TEXT("\n");
+			FileHandle->Write(reinterpret_cast<const uint8*>(*LineWithNewLine), LineWithNewLine.Len());
+		}
+
+		// Dosyayı kapatma
+		delete FileHandle;
+
+		// Dosya başarıyla kaydedildi
+		return true;
+	}
+
+	// Dosya açılamadı veya yazma hatası oluştu
+	return false;
+}
+
+TArray<FString> UMephistoSave::ReadDataFromFile(const FString& Directory, const FString& FileName)
+{
+	TArray<FString> ReadData;
+
+	FString FullPath = FPaths::Combine(Directory, FileName);
+
+	// Dosyayı okuma işlemi
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+
+	// Binary dosyasını okuma modunda açma
+	IFileHandle* FileHandle = PlatformFile.OpenRead(*FullPath);
+	if (FileHandle)
+	{
+		// Dosyayı tamamen okuma işlemi
+		uint32 FileSize = FileHandle->Size();
+		TArray<uint8> FileContent;
+		FileContent.SetNumUninitialized(FileSize);
+		FileHandle->Read(FileContent.GetData(), FileSize);
+
+		// Dosyayı kapatma
+		delete FileHandle;
+
+		// Dosyadan okunan veriyi FString'e dönüştürme
+		FString FileContentStr = FString(reinterpret_cast<const TCHAR*>(FileContent.GetData()), FileSize);
+
+		// FString'i satırlara ayırma
+		TArray<FString> Lines;
+		FileContentStr.ParseIntoArrayLines(Lines, true);
+
+		// Okunan satırları çıktı olarak döndürme
+		ReadData = MoveTemp(Lines);
+	}
+
+	return ReadData;
+}
+
--- Source/Unreal_Crafting/Public/MephistoSave.h
+++ Source/Unreal_Crafting/Public/MephistoSave.h
@@ -16,6 +16,11 @@ class UNREAL_CRAFTING_API UMephistoSave : public UBlueprintFunctionLibrary
 
 	UFUNCTION(BlueprintCallable, Category = "Mephisto Save")
 	static bool WriteFile(const FString& Directory, const FString& FileName, const FString& Data);
+	
+	UFUNCTION(BlueprintCallable, Category = "Mephisto Save")
+	static FString BytesArrayConvertToString(TArray<uint8> data);
+
+	static FString StringFromBytes(const TArray<uint8>& Bytes);
 
 	UFUNCTION(BlueprintCallable, Category = "Mephisto Save")
 	static FString ReadFile(const FString& Directory, const FString& FileName, FString& OutData);
@@ -25,8 +30,18 @@ class UNREAL_CRAFTING_API UMephistoSave : public UBlueprintFunctionLibrary
 
 	UFUNCTION(BlueprintCallable, Category = "Mephisto Save")
 	static TArray<uint8> Serializeable(AActor* MyActor);
+	
+	UFUNCTION(BlueprintCallable, Category = "Mephisto Save")
+	static TArray<uint8> Serializeables(TArray<AActor*> MyActors);
 
 	UFUNCTION(BlueprintCallable, Category = "Mephisto Save")
-	static void Deserializeable(const TArray<uint8>& SerializedData);
+	static AActor* Deserializeable(TArray<UClass*> classType, UWorld* World, const TArray<uint8>& SerializedData);
+
+	UFUNCTION(BlueprintCallable, Category = "Mephisto Save")
+	static bool WriteDataToFile(const FString& Directory, const FString& FileName, const TArray<FString>& Data);
+
+	UFUNCTION(BlueprintCallable, Category = "Mephisto Save")
+	static TArray<FString> ReadDataFromFile(const FString& Directory, const FString& FileName);
+
 
 };
